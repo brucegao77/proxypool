d35605b2819cdd0e22d1a8972bc77af262d3ab7d
Your Name <775699650@qq.com>
2018-12-26 16:32:49 +0800

    更新

--- file.py
+++ file.py
@@ -1,15 +1,16 @@
 import requests
 from pymongo import MongoClient
+from concurrent.futures import ThreadPoolExecutor
 
 client = MongoClient(host="127.0.0.1", port=27017)
 db = client['proxypool']
-collection = db['proxies']
+collection = db['test']
 
 
 # 从提取文件中导入proxies
 def get_proxy():
     data = []
-    for line in open("proxy.txt", "r"):
+    for line in open("proxies.txt", "r"):
         data.append(line[:-1])
     print("总数：", len(data))
     return data
@@ -20,29 +21,25 @@ def test_proxy(proxy):
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5'
     }
-    type = 'https'
     ip = {
         "https": "https://" + proxy
     }
     try:
-        res = requests.get('https://icanhazip.com', headers=headers, proxies=ip, timeout=5)
+        res = requests.get('https://www.baidu.com/', headers=headers, proxies=ip, timeout=5)
         if res.status_code == 200:
-            proxies = type + "://" + proxy
             items = {
-                'proxies': proxies
+                'proxies': "https://" + proxy
             }
-            if collection.insert(items):
-                print('有效', res.status_code, res.text)
+            collection.insert(items)
+            print('有效', proxy)
     except:
         print('无效')
         pass
 
-
-# 存入数据库
-count = 0
+# 多线程
 data = get_proxy()
+executor = ThreadPoolExecutor(max_workers=2)
 for proxy in data:
-    count += 1
-    print('====== 当前验证第{}个 ======'.format(count))
-    test_proxy(proxy)
+    all_task = [executor.submit(test_proxy, (proxy))]
+
 
---
+++ xicidaili.py
@@ -0,0 +1,63 @@
+import requests
+from lxml import etree
+from concurrent.futures import ThreadPoolExecutor
+import time
+from pymongo import MongoClient
+
+client = MongoClient(host="127.0.0.1", port=27017)
+db = client['proxypool']
+collection = db['test']
+
+# 爬取西刺代理
+def get_proxy():
+    data = []
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.26 Safari/537.36 Core/1.63.6788.400 QQBrowser/10.3.2864.400',
+        'referer': 'https://www.xicidaili.com/'
+    }
+    proxies = {
+        'https': 'https://217.19.209.253:8080'
+    }
+    for i in range(1, 5):
+        url = 'https://www.xicidaili.com/nn/{}'.format(i)
+        res = requests.get(url, headers=headers, proxies=proxies, timeout=5)
+        print(res.status_code)
+        data = etree.HTML(res.text)
+        time.sleep(2)
+
+        contents = data.xpath('//tr[@class]')  # 去掉标题行
+        for content in contents:
+            ip = content.xpath('./td[2]/text()')[0]
+            port = content.xpath('./td[3]/text()')[0]
+            type = content.xpath('./td[6]/text()')[0]
+            proxy = '{0}://{1}:{2}'.format(type, ip, port)
+            print(proxy)
+            data.append(proxy)
+    return data
+
+# 验证proxy的可用性并存入数据库
+def test_proxy(proxy):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5'
+    }
+    ip = {
+        "https": "https://" + proxy
+    }
+    try:
+        res = requests.get('https://www.baidu.com/', headers=headers, proxies=ip, timeout=5)
+        if res.status_code == 200:
+            items = {
+                'proxies': "https://" + proxy
+            }
+            collection.insert(items)
+            print('有效', proxy)
+    except:
+        print('无效')
+        pass
+
+# 多线程
+data = get_proxy()
+print(data)
+executor = ThreadPoolExecutor(max_workers=2)
+for proxy in data:
+    all_task = [executor.submit(test_proxy, (proxy))]
